dfb7f77aeb820cf4188746bfd9fd52ebcc0da4b5
roseokpe1~ <roseokpe@gmail.com>
2023-05-30 05:26:06 -0700

    allow to itierate questions

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+import Question from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+
+  const [questions, setQuestions] = useState(data)
+  return <main>
+        <section className="info">
+      {
+        questions.map((questions) => {
+   return <SingleQuestion key={questions.id} {...questions} />
+      })
+    }
+    </section>
+    </main>;
 }
 
 export default App;
